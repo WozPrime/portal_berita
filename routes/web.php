847c80cff3dc91bc59d5d6c9b5bad9821d73a59e
WozPrime <fakhri_ishan@student.untan.ac.id>
2021-12-13 16:10:51 +0700

    Add route logout

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout']);
 
 Route::resource('/comment', UserController::class);
 
